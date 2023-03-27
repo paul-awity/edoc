a2cc8490446b7f2afc86ca0aefbd93f22cf46833
Paul Awity <33389345+paul-awity@users.noreply.github.com>
2023-03-27 16:49:35 +0300

    Debugging
    Debugging

--- patient/schedule.php
+++ patient/schedule.php
@@ -44,6 +44,8 @@
     $stmt->bind_param("s",$useremail);
     $stmt->execute();
     $result = $stmt->get_result();
+    include("../connection.php");
+    $userrow = $database->query("select * from patient where pemail='$useremail'");
     $userfetch=$userrow->fetch_assoc();
     $userid= $userfetch["pid"];
     $username=$userfetch["pname"];
